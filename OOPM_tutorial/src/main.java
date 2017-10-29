4dc5162ffc2b4549444ffa489cca70ea31ada99f
mnonn <blumenkind2005@gmail.com>
2017-10-29 14:00:07 +0100

    added console input

--- OOPM_tutorial/src/main.java
+++ OOPM_tutorial/src/main.java
@@ -1,9 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.String;
 
 public class main {
 
-	public static void main(String[] args) {
-		int result= Functionality.quersumme(10);
-		System.out.println(result);
+	public static void main (String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter Integer: ");
+        try{
+            String input = br.readLine();            
+            int i = Integer.parseInt(input);
+            System.out.println(Functionality.quersumme(i));
+        }catch(NumberFormatException nfe){
+            System.err.println("Invalid Format!");
+        }catch(IOException io){
+            System.err.println("Invalid Input!");
+        }
 	}
 
 }
